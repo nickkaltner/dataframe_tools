4d1e0ef7018ea784483452535cb41ece7c208623
Nick Kaltner <822124+nickkaltner@users.noreply.github.com>
2024-04-25 15:06:33 +1000

    initial commit

---
+++ lib/dataframe_tools.ex
@@ -0,0 +1,18 @@
+defmodule DataframeTools do
+  @moduledoc """
+  Documentation for `DataframeTools`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DataframeTools.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/dataframe_tools_test.exs
@@ -0,0 +1,8 @@
+defmodule DataframeToolsTest do
+  use ExUnit.Case
+  doctest DataframeTools
+
+  test "greets the world" do
+    assert DataframeTools.hello() == :world
+  end
+end
